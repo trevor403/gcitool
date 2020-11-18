475cc3698110f0cb0979a8b81a4609288c5ca85c
Trevor Rudolph <cool.test.cool@gmail.com>
2020-11-18 05:07:59 -0500

    extra validations

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <LibPkmGC/Colosseum/SaveEditing/Save.h>
 #include <LibPkmGC/XD/SaveEditing/Save.h>
 
+// #include <LibPkmGC/GC/Common/Pokemon.h>
+
 using namespace LibPkmGC;
 
 const size_t GCN_COLOSSEUM_GCI_SIZE = 0x60040;
@@ -64,18 +66,25 @@ int main(int argc, char *argv[]) {
         std::ofstream save_slot_stream(save_slot_path, std::ofstream::binary);
         save_slot_stream.write((const char*)save_slot->data, save_slot_size);
         save_slot_stream.close();
-        printf("extracted save slot to %s\n", save_slot_path.c_str());
+
+        const char *status = save_slot->isCorrupt() ? "corrupt" : "valid";
+        printf("extracted %s save slot to %s\n", status, save_slot_path.c_str());
     }
 
     if (edit_mode == REPLACE) {
         std::ifstream save_slot_stream(save_file_path, std::ios::in | std::ios::binary);
         std::vector<uint8_t> save_slot_buf((std::istreambuf_iterator<char>(save_file_stream)), std::istreambuf_iterator<char>());
         save_slot->reload(save_slot_buf.data(), 1); // is decrypted
-        save->saveEncrypted(save_file_buf.data(), true);
+        // save_slot->player->trainer->trainerName = new GC::PokemonString("TEST");
+        save_slot->checkBothChecksums(true, true);
+        save->saveEncrypted(save_file_buf.data(), true, true);
 
         save_file_stream.seekp(0, std::ios::beg);
-        save_file_stream.write((const char*)save->data, save_size);
-        printf("replaced save slot in %s\n", save_file_path.c_str());
+        save_file_stream.write((const char*)save_file_buf.data(), save_size);
+        save_file_stream.close();
+
+        const char *status = save_slot->isCorrupt() ? "corrupt" : "valid";
+        printf("replaced %s save slot in %s\n", status, save_file_path.c_str());
     }
 
     return 0;
